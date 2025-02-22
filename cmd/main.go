7b7f2025732d6a9221b3bbb0295a265835a89b0a
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-02-21 21:56:22 -0500

    update main package to collect end point data.

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/NathanSanchezDev/go-insight/internal/api"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+
+	metricsHandler := api.NewMetricsHandler()
+
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(200, gin.H{"status": "ok"})
 	})
+
+	r.POST("/collect", metricsHandler.CollectMetrics)
+
 	r.Run(":8080")
 }
