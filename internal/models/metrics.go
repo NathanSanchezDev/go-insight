afa435d15ed917909344874fc6583a8648cef721
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-03-02 22:45:35 -0500

    update metrics model.

--- internal/models/metrics.go
+++ internal/models/metrics.go
@@ -3,21 +3,20 @@ package models
 import "time"
 
 type MetricSource struct {
-	Langauge  string `json:"language"`
+	Language  string `json:"language"`
 	Framework string `json:"framework"`
 	Version   string `json:"version"`
 }
 
 type EndpointMetric struct {
-	Path       string  `json:"path"`
-	Method     string  `json:"method"`
-	StatusCode int     `json:"status_code"`
-	Duration   float64 `json:"duration_ms"`
-
+	ID          int          `json:"id"`
 	ServiceName string       `json:"service_name"`
+	Path        string       `json:"path"`
+	Method      string       `json:"method"`
+	StatusCode  int          `json:"status_code"`
+	Duration    float64      `json:"duration_ms"`
 	Source      MetricSource `json:"source"`
 	Environment string       `json:"environment"`
-
-	Timestamp time.Time `json:"timestamp"`
-	RequestID string    `json:"request_id"`
+	Timestamp   time.Time    `json:"timestamp"`
+	RequestID   string       `json:"request_id"`
 }
