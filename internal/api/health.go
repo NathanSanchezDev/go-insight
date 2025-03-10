49aee699f966439dc1a3c9876aca1d73147913ea
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-03-09 23:39:02 -0400

    remove comment

--- internal/api/health.go
+++ internal/api/health.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 )
 
-// HealthCheck is a simple endpoint to check if the server is running
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
