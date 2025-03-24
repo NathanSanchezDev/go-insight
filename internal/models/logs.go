42dd20c65db5c275c98cccdabb6847841d55f12a
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-03-23 20:04:54 -0400

    fix metadata struct

--- internal/models/logs.go
+++ internal/models/logs.go
@@ -2,16 +2,17 @@ package models
 
 import (
 	"database/sql"
+	"encoding/json"
 	"time"
 )
 
 type Log struct {
-	ID          int            `json:"id"`
-	ServiceName string         `json:"service_name"`
-	LogLevel    string         `json:"log_level"`
-	Message     string         `json:"message"`
-	Timestamp   time.Time      `json:"timestamp"`
-	TraceID     sql.NullString `json:"trace_id,omitempty"`
-	SpanID      sql.NullString `json:"span_id,omitempty"`
-	Metadata    sql.NullString `json:"metadata,omitempty"`
+	ID          int             `json:"id"`
+	ServiceName string          `json:"service_name"`
+	LogLevel    string          `json:"log_level"`
+	Message     string          `json:"message"`
+	Timestamp   time.Time       `json:"timestamp"`
+	TraceID     sql.NullString  `json:"trace_id,omitempty"`
+	SpanID      sql.NullString  `json:"span_id,omitempty"`
+	Metadata    json.RawMessage `json:"metadata,omitempty"`
 }
