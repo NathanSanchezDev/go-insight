d815403fa13f8fe202087cc76e004aeca24c9665
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-03-23 22:39:55 -0400

    Organize main file, abstracting routing and create middleware connection method.

--- cmd/main.go
+++ cmd/main.go
@@ -4,22 +4,42 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/NathanSanchezDev/go-insight/internal/api"
 	"github.com/NathanSanchezDev/go-insight/internal/db"
-	"github.com/gorilla/mux"
 )
 
 func main() {
 	db.InitDB()
+	router := api.SetupRoutes()
+	router.Use(loggingMiddleware)
+	port := getEnvPort(8080)
 
-	router := mux.NewRouter()
-	router.HandleFunc("/metrics", api.GetMetricsHandler).Methods("GET")
-	router.HandleFunc("/logs", api.GetLogsHandler).Methods("GET")
-	router.HandleFunc("/metrics", api.PostMetricHandler).Methods("POST")
-	router.HandleFunc("/logs", api.PostLogHandler).Methods("POST")
-
-	port := 8080
+	// Start server
 	fmt.Printf("🚀 Server started on port %d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
+}
+
+// Middleware for logging requests
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		log.Printf("%s %s %s", r.Method, r.RequestURI, time.Since(start))
+	})
+}
+
+// Get port from environment variable or use default
+func getEnvPort(defaultPort int) int {
+	portStr := os.Getenv("PORT")
+	if portStr != "" {
+		port, err := strconv.Atoi(portStr)
+		if err == nil {
+			return port
+		}
+	}
+	return defaultPort
 }
