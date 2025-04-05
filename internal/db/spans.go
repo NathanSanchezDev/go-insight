aab62a6808edd6b64b955060ab10def07041f2bb
Nathan Sanchez <nathansanchezdev@outlook.com>
2025-04-05 14:11:37 -0400

    swap functions in files for organization.

--- internal/db/spans.go
+++ internal/db/spans.go
@@ -6,44 +6,44 @@ import (
 	"github.com/NathanSanchezDev/go-insight/internal/models"
 )
 
-func StoreTrace(trace models.Trace) error {
-	query := `INSERT INTO traces (id, service_name, start_time) VALUES ($1, $2, $3)`
-	_, err := DB.Exec(query, trace.ID, trace.ServiceName, trace.StartTime)
+func StoreSpan(span models.Span) error {
+	query := `INSERT INTO spans (id, trace_id, parent_id, service, operation, start_time) VALUES ($1, $2, $3, $4, $5, $6)`
+	_, err := DB.Exec(query, span.ID, span.TraceID, span.ParentID, span.Service, span.Operation, span.StartTime)
 	if err != nil {
-		log.Println("Failed to store trace:", err)
+		log.Println("Failed to store span:", err)
 	}
 	return err
 }
 
-func UpdateTrace(trace *models.Trace) error {
-	query := `UPDATE traces SET end_time = $1, duration_ms = $2 WHERE id = $3`
-	_, err := DB.Exec(query, trace.EndTime, trace.Duration, trace.ID)
+func UpdateSpan(span *models.Span) error {
+	query := `UPDATE spans SET end_time = $1, duration_ms = $2 WHERE id = $3`
+	_, err := DB.Exec(query, span.EndTime, span.Duration, span.ID)
 	if err != nil {
-		log.Println("Failed to update trace:", err)
+		log.Println("Failed to update span:", err)
 	}
 	return err
 }
 
-func FetchTraces(service string) ([]models.Trace, error) {
-	query := `SELECT id, service_name, start_time, end_time, duration_ms FROM traces WHERE service_name = $1 ORDER BY start_time DESC`
-	rows, err := DB.Query(query, service)
+func FetchSpans(traceID string) ([]models.Span, error) {
+	query := `SELECT id, trace_id, parent_id, service, operation, start_time, end_time, duration_ms FROM spans WHERE trace_id = $1 ORDER BY start_time`
+	rows, err := DB.Query(query, traceID)
 	if err != nil {
-		log.Println("Failed to fetch traces:", err)
+		log.Println("Failed to fetch spans:", err)
 		return nil, err
 	}
 	defer rows.Close()
 
-	var traces []models.Trace
+	var spans []models.Span
 	for rows.Next() {
-		var trace models.Trace
-		err := rows.Scan(&trace.ID, &trace.ServiceName, &trace.StartTime, &trace.EndTime, &trace.Duration)
+		var span models.Span
+		err := rows.Scan(&span.ID, &span.TraceID, &span.ParentID, &span.Service, &span.Operation, &span.StartTime, &span.EndTime, &span.Duration)
 		if err != nil {
-			log.Println("Error scanning trace row:", err)
+			log.Println("Error scanning span row:", err)
 			continue
 		}
-		traces = append(traces, trace)
+		spans = append(spans, span)
 	}
-	return traces, nil
+	return spans, nil
 }
 
 func FetchSpanByID(spanID string) (models.Span, error) {
